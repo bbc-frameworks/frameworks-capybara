0b614b8bcce961698a29c825b557ac8cc41c4fc8
mcrmfc <matthew.robbins@bbc.co.uk>
2011-05-09 16:55:34 +0100

    adding patch for capybara-mechanize to set url correctly when form has empty action

---
+++ lib/monkey-patches/capybara-mechanize-patches.rb
@@ -0,0 +1,29 @@
+require 'capybara/mechanize/cucumber' 
+
+class Capybara::Driver::Mechanize
+  def process_remote_request(method, url, *options)
+    if remote?(url)
+      remote_uri = URI.parse(url)
+      if remote_uri.host.nil?
+        #TODO: Ascertain whether this is really true...
+        if(method == :post && @prev_url) #patch
+          #require 'uri'
+          #url = "http://#{URI.parse(@prev_url).host}#{URI.parse(@prev_url).path}"
+          #p url
+          url = @prev_url #patch
+        else
+          remote_host = @last_remote_host || Capybara.app_host || Capybara.default_host
+          url = File.join(remote_host, url)
+          url = "http://#{url}" unless url.include?("http")
+        end
+      else
+        @last_remote_host = "#{remote_uri.host}:#{remote_uri.port}"
+      end
+      @prev_url = url #patch
+      reset_cache
+      @agent.send *( [method, url] + options)
+
+      @last_request_remote = true
+    end
+  end
+end
