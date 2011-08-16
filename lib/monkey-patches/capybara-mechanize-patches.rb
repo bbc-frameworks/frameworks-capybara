24b7dc37793346fceaefb57111f73fc60bef31a3
mcrmfc <matthew.robbins@bbc.co.uk>
2011-08-16 14:28:24 +0100

    update patches for mechanize driver to cope with https and where urls contain http in middle of string

--- lib/monkey-patches/capybara-mechanize-patches.rb
+++ lib/monkey-patches/capybara-mechanize-patches.rb
@@ -1,12 +1,16 @@
 require 'capybara/mechanize/cucumber' 
+require 'uri'
 
 class Capybara::Driver::Mechanize
   def process_remote_request(method, url, *options)
     if remote?(url)
       remote_uri = URI.parse(url)
+    
+      @scheme = remote_uri.scheme if remote_uri.scheme 
+
       if remote_uri.host.nil?
         #TODO: Ascertain whether this is really true...
-        if(method == :post && @prev_url) #patch
+        if(method == :post && url == "" && @prev_url) #patch
           #require 'uri'
           #url = "http://#{URI.parse(@prev_url).host}#{URI.parse(@prev_url).path}"
           #p url
@@ -14,7 +18,8 @@ class Capybara::Driver::Mechanize
         else
           remote_host = @last_remote_host || Capybara.app_host || Capybara.default_host
           url = File.join(remote_host, url)
-          url = "http://#{url}" unless url.include?("http")
+          #url = "http://#{url}" unless url.include?("http")
+          url = "#{@scheme}://#{url}" unless url.match(/^http.*/)
         end
       else
         @last_remote_host = "#{remote_uri.host}:#{remote_uri.port}"
