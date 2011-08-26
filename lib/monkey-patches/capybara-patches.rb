ba978ff01a87a58067a90d59d66d9ac6402341cc
unknown <hassaa10@.national.core.bbc.co.uk>
2011-08-26 09:00:22 +0100

    Monkey patch code for cookie handling in Mechanize and Webdriver

--- lib/monkey-patches/capybara-patches.rb
+++ lib/monkey-patches/capybara-patches.rb
@@ -21,3 +21,61 @@ class Capybara::Driver::Node
   end
 end
 
+class Capybara::Driver::Selenium
+  def cookies
+    browser.manage.all_cookies
+  end
+  
+  def cookie_named(name)
+    browser.manage.cookie_named(name)
+  end
+  
+  def delete_cookie(cookie)
+    browser.manage.delete_cookie(cookie)
+  end
+  
+  def delete_all_cookies
+    browser.manage.delete_all_cookies
+  end
+  
+  def add_cookie(attribs)
+    browser.manage.add_cookie(attribs)
+  end
+end
+
+module Capybara
+  class Session
+    ##
+    # Get all cookies
+    #
+    # @return [Array<Hash>] list of cookies
+    #
+    def cookies    
+      driver.cookies
+    end
+
+    ##
+    # Get the cookie with the given name
+    #
+    # @param [String] name the name of the cookie
+    # @return [Hash, nil] the cookie, or nil if it wasn't found.
+    #
+    def cookie_named(name)    
+      driver.cookie_named(name)
+    end
+    
+    def delete_cookie(cookie)
+      driver.delete_cookie(cookie)
+    end
+    
+    def delete_all_cookies
+      driver.delete_all_cookies
+    end
+    
+    def add_cookie(attribs)
+      driver.add_cookie(attribs)
+    end
+  end
+end
+
+
