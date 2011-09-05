ff3e3bb2a3ef490177b1b0d0a8ac6e393b587f60
mcrmfc <matthew.robbins@bbc.co.uk>
2011-09-05 12:37:08 +0100

    add schemas for local xhtml validation

--- lib/monkey-patches/capybara-patches.rb
+++ lib/monkey-patches/capybara-patches.rb
@@ -1,6 +1,7 @@
 require 'selenium-webdriver'
 require 'capybara/cucumber'
 
+##
 #Monkey Patch's - Use with care!
 #
 class Capybara::Driver::Selenium::Node
@@ -25,19 +26,19 @@ class Capybara::Driver::Selenium
   def cookies
     browser.manage.all_cookies
   end
-  
+
   def cookie_named(name)
     browser.manage.cookie_named(name)
   end
-  
+
   def delete_cookie(cookie)
     browser.manage.delete_cookie(cookie)
   end
-  
+
   def delete_all_cookies
     browser.manage.delete_all_cookies
   end
-  
+
   def add_cookie(attribs)
     browser.manage.add_cookie(attribs)
   end
@@ -63,18 +64,41 @@ module Capybara
     def cookie_named(name)    
       driver.cookie_named(name)
     end
-    
+
     def delete_cookie(cookie)
       driver.delete_cookie(cookie)
     end
-    
+
     def delete_all_cookies
       driver.delete_all_cookies
     end
-    
+
     def add_cookie(attribs)
       driver.add_cookie(attribs)
     end
+
+    DTD_REFS = {
+      "-//W3C//DTD XHTML 1.0 Strict//EN" => {:dtd => "#{File.dirname( __FILE__)}/../../schemas/xhtml1-strict.dtd", :type => :strict},
+      "-//W3C//DTD XHTML 1.0 Transitional//EN" => {:dtd => "#{File.dirname( __FILE__)}/../../schemas/xhtml1-transitional.dtd", :type => :transitional}, 
+      "-//W3C//DTD XHTML+RDFa 1.0//EN" => {:dtd => "#{File.dirname( __FILE__)}/../../schemas/xhtml-rdfa-1.dtd", :type => :rdfa}}
+
+      def validate(source)
+        doctype = source.scan(/\"(.*?)\"/)[0].to_s 
+
+        if(DTD_REFS[doctype])
+          dtd = DTD_REFS[doctype][:dtd]
+          type = DTD_REFS[doctype][:type]
+        end
+
+        raise "RDFA Validation not currently supported due to issues in Nokogiri" if type == :rdfa
+
+        source = source.gsub(/PUBLIC \"-\/\/W3C\/\/DTD XHTML.*?\/\/EN\" \"http:\/\/www.w3.org.*?\"/, "SYSTEM \"#{dtd}\"")
+        doc = Nokogiri::XML(source) { |cfg|
+          cfg.noent.dtdload.dtdvalid
+        }
+        errors = doc.validate
+        raise "Page (#{current_url}) failed XHTML vaidation (or Nokogiri Freaked out...please manually check against W3C), errors:#{errors.to_s}" unless errors == []
+      end
   end
 end
 
