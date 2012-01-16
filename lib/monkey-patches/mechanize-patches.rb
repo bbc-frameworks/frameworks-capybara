174175bf7ce56c2019fef808cef75667347c079b
mcrmfc <matthew.robbins@bbc.co.uk>
2012-01-16 16:50:22 +0000

    update mechanize monkey patch to make it behave for cookies where host contains port

--- lib/monkey-patches/mechanize-patches.rb
+++ lib/monkey-patches/mechanize-patches.rb
@@ -4,4 +4,15 @@ class Mechanize
     @cert, @key = clientcert, clientkey
     @ca_file = cacert if cacert
   end
+
+  class CookieJar
+    #hopefuly this alias approach will mean we capture changes in the mechanize method 
+    alias_method :old_add, :add
+    def add(uri, cookie)
+      uri.host = uri.host.gsub(/:.*/,"") #if host contains a port remove it so cookie validation works e.g. sandbox:6081
+      old_add(uri, cookie)
+    end
+  end
+
 end
+
