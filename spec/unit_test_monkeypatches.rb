19ea4b358d9deea309dd6b7c9f3ea2bff2a943c0
mcrmfc <matthew.robbins@bbc.co.uk>
2012-04-27 12:33:20 +0100

    fix issues with ff profile stubbing

--- spec/unit_test_monkeypatches.rb
+++ spec/unit_test_monkeypatches.rb
@@ -7,23 +7,7 @@ module Selenium
         module_function
 
         def app_data_path
-          p "in patch"
-          testpath = File.dirname(__FILE__)
-          case Platform.os
-          when :windows
-            "#{ENV['APPDATA']}\\Mozilla\\Firefox"
-          when :macosx
-            #"#{Platform.home}/Library/Application Support/Firefox"
-            testpath
-          when :unix, :linux
-            "#{Platform.home}/.mozilla/firefox"
-          else
-            raise "Unknown os: #{Platform.os}"
-          end
-        end
-
-        def stringified?(str)
-          str =~ /^".*"$/
+           File.dirname(__FILE__)
         end
 
       end # Util
