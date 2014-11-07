a2add5c8e30ad97c9621591f405eced1d29e690e
Glen Birnie <birnig01@mc-s056704.local>
2014-11-07 15:57:06 +0000

    Restored unit testing monkeypatch (required to force Firefox to locally create named profiles).

---
+++ spec/unit_test_monkeypatches.rb
@@ -0,0 +1,20 @@
+# This patch overrides the normal mechanism for searching for Firefox profiles
+# causing named profiles to be created in the local directory (rather than be referenced)
+# for unit/integration testing purposes
+module Selenium
+  module WebDriver
+    module Firefox
+
+      # @api private
+      module Util
+        module_function
+
+        def app_data_path
+           File.dirname(__FILE__)
+        end
+
+      end # Util
+    end # Firefox
+  end # WebDriver
+end # Selenium
+
